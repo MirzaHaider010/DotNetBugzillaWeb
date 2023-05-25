6e8c55b0c9970c4001782171311e3d3ec05a18c0
DESKTOP-FJLKLLR\Dev <haider.ejaz@devsinc.com>
2023-05-25 13:11:56 -0500

    Initial Commit

---
+++ BugzillaBackend/BugzillaBackend/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BugzillaBackend
+{
+    public class UserDto // User Data Transfer Object
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
